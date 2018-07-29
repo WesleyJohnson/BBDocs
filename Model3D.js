3e0869222111cf525bc84e6a1a8130cfffab30de
Wesley Johnson <johnson.wesley.t@gmail.com>
2018-07-29 16:05:45 -0400

    Add Model3D.

---
+++ Model3D.js
@@ -0,0 +1,27 @@
+class Model3D {
+    /*
+    @param: x number
+    @param: y number
+    @param: z number
+    */
+    setPosition(x, y, z){}
+
+    /*
+    @param: x number
+    @param: y number
+    @param: z number
+    */
+    setRotation(x, y, x){}
+
+    /*
+    @param: x number
+    @param: y number
+    @param: z number
+    */
+    setScale(x, y, z){}
+
+    /*
+    @param: name
+    */
+    setTexture(name){}
+}
